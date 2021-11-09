e209dffafa154a0aa9aa7165722e610c1842c5f2
blubbel <paul@jippa.de>
2021-11-09 16:41:28 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("./shops/clothshop");
+// require("./shops/clothshop");
 
 mp.events.addProc("isMale", () => {
     return mp.players.local.isMale();
@@ -27,32 +27,28 @@ mp.events.add("render", () => {
 });
 
 mp.events.add("playerReady", () => {
-    mp.game.invoke("0xB1906895227793F3", 0);
-    mp.game.invoke("0x29961D490E5814FD", mp.players.local.handle, 2000);
-    mp.game.invoke("0x5C8B2F450EE4328E", mp.players.local.handle, true);
+    mp.game.invoke("0xB1906895227793F3", 3);
 });
 
 
 mp.events.add('playerWeaponShot', (targetPosition, targetEntity) => {
-    mp.game.invoke("0xB1906895227793F3", 0);
-    mp.game.invoke("0x29961D490E5814FD", mp.players.local.handle, 2000);
-    mp.game.invoke("0x5C8B2F450EE4328E", mp.players.local.handle, true);
+    mp.game.invoke("0xB1906895227793F3", 3);
 });
 
 function weaponSwitch() {
-    if (lastWeapon === 0x0A3D4D34) {
-        var componentModel = mp.game.weapon.getWeaponComponentTypeModel(0x7BC4CDDC);
+    if (lastWeapon === 0x61012683) {
+        const componentModel = mp.game.weapon.getWeaponComponentTypeModel(0x7BC4CDDC);
         RequestModel(componentModel, 1000, function () {
-            mp.game.invoke("0xD966D51AA5B28BB9", mp.players.local.handle >> 0, 0x0A3D4D34, 0x7BC4CDDC); // PDW Flashlight
+            mp.game.invoke("0xD966D51AA5B28BB9", mp.players.local.handle >> 0, 0x61012683, 0xEAC8C270); // COMPONENT_GUSENBERG_CLIP_02
         });
     }
 
     mp.players.forEachInStreamRange(player => {
 
-        if (player.weapon === 0x0A3D4D34) {
-            var componentModel = mp.game.weapon.getWeaponComponentTypeModel(0x7BC4CDDC);
+        if (player.weapon === 0x61012683) {
+            const componentModel = mp.game.weapon.getWeaponComponentTypeModel(0x7BC4CDDC);
             RequestModel(componentModel, 1000, function () {
-                mp.game.invoke("0xD966D51AA5B28BB9", player.handle >> 0, 0x0A3D4D34, 0x7BC4CDDC); // PDW Flashlight
+                mp.game.invoke("0xD966D51AA5B28BB9", player.handle >> 0, 0x61012683, 0xEAC8C270); // COMPONENT_GUSENBERG_CLIP_02
             });
         }
     });
@@ -81,3 +77,26 @@ function RequestModel(model, ms, callback) {
         callback();
     }
 }
+
+mp.events.add('outgoingDamage', (sourceEntity, targetEntity, targetPlayer, c_weapon, boneIndex, damage) => {
+    if (mp.players.local.isPerformingStealthKill()) {
+        return true;
+    }
+
+    if (targetEntity.type === "player" && targetPlayer != null) {
+        if (mp.players.local.isInAnyVehicle(false) && !targetPlayer.isInAnyVehicle(false)) {
+            if (!targetEntity.isSwimmingUnderWater() && !targetEntity.isSwimming()) {
+                return true;
+            }
+        }
+
+        if (c_weapon == 0x1D073A89) {
+            mp.events.callRemote("playerShotPlayer", [targetPlayer.name, Number(c_weapon), boneIndex]);
+            if (boneIndex === 20) {
+                return true;
+            }
+        } else {
+            mp.events.callRemote("playerShotPlayer", [targetPlayer.name, Number(c_weapon), boneIndex]);
+        }
+    }
+});
